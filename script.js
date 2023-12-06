4c793e2c2e6df8d4299cd8c6eacb94e838104496
jmoussalli <jerome.moussalli@gmail.com>
2023-12-06 15:15:25 +0100

    last one

--- script.js
+++ script.js
@@ -2,18 +2,6 @@ function add(a,b){
     return a+b;
 }
 
-function subtract(a,b){
-    return a-b;
-}
-
-function multiply(a,b){
-    return a*b;
-}
-
-function divide(a,b){
-    return a/b;
-}
-
-function nouvelle(){
-    console.log("hello !");
+function abc(){
+    console.log("ABC");
 }
